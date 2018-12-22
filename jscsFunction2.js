8e65c717561c455ac3df98480da19d9454d241a6
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-12-22 12:49:29 -0800

    Array length recursively and ES6

--- jscsFunction2.js
+++ jscsFunction2.js
@@ -49,33 +49,72 @@ console.log('Challenge 14:', uniqueArr);
 // Challenge 15
 // Recursive sum
 const sumRange = function(n){
-  let result = 0;
-  if(n = 1){return;}
-  result = sumRange(n) + sumRange(n-1);
+  if(n === 1){return 1;}
+  else {return n + sumRange(n - 1);}
 }
-
 
 // Uncomment the line below to test your code
 console.log('Challenge 15:', sumRange(3));
 // should print 'Challenge 15: 6'
 
+
 // Challenge 16
-// Write your code here
+  
+//native array.length method
+const getLength = function(array){
+  return array.length;
+}
+
+// Using counter
+const getLength = function(array){   
+   let count = 0;
+   for(let i of array){count += 1;}
+   return count;
+}
+
+ //Using recursion with ...thisArg
+const getLength = ([first, ...last]) => { 
+  if (typeof(first) === 'undefined') {return 0;}
+  else {return 1 + getLength(last)} 
+}
+
+//Using recursion checking next element of array
+const getLength = function(array, index){   
+  index = index || 0;
+  if(array[index + 1]){
+    return getLength(array, index + 1);
+  }
+  else if(index === 0){return 0;}
+  else {return index + 1;}
+}
+
 // Uncomment the line below to test your code
 console.log('Challenge 16:', getLength(['x', 'y', 'z']));
 // should print 'Challenge 16: 3'
 
 
 // Challenge 17
-// Write your code here
+// Using ES5
+const isEven = function(n){
+  if(n % 2 === 0){return true;}
+  else return false;
+}
+
+//Using ES6
+const isEven = n => (n % 2 === 0)? true : false;
+
 // Uncomment the line below to test your code
-// console.log('Challenge 17:', isEven(5));
+console.log('Challenge 17:', isEven(5));
 // should print 'Challenge 17: false'
 
 
 // Challenge 18
 // Write your code here
+const multiplier = (array, n) => array.map(x => x * n);
+
+
+
 // Uncomment the line below to test your code
-// console.log('Challenge 18:', multiplier([1, 2, 3], 3));
+console.log('Challenge 18:', multiplier([1, 2, 3], 3));
 // should print 'Challenge 18: [ 3, 6, 9 ]'
 
